9ba984d49a20a446ffcf51355ea9a8c700dff299
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-11 23:44:04 +0530

    playerRank program commit

---
+++ src/playerRank.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class playerRank {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of players in the contest : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the scores of the players : ");
+        for(int i = 0 ; i < n ; i++)
+            arr[i] = sc.nextInt();
+
+        // Now we will find the Winner , second winner , loser and the second loser
+
+
+
+
+    }
+}
