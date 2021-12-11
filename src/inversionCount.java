1692507cd41159be41079ee0b78163fc8c00fb40
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-12 00:05:25 +0530

    InversionCount program commit

---
+++ src/inversionCount.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class inversionCount {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements in the array : ");
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+        int inversioncount = 0;
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]>arr[j])
+                    inversioncount++;
+            }
+        }
+    }
+}
