d14b0e66aa493349dc105425047fbb5a5ad1f95e
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-31 16:12:37 +0530

    Binary tree random node commit

--- src/Opengenus_tree_random_node.java
+++ src/Opengenus_tree_random_node.java
@@ -1,14 +1,12 @@
 // Using auxillary array to find the random node in a given binary tree
 public class Opengenus_tree_random_node {
-    static class TreeNode
-    {
+    static class TreeNode {
         int val;
         int child;
         TreeNode left, right;
     }
 
-    static TreeNode Node(int data)
-    {
+    static TreeNode Node(int data) {
         TreeNode temp = new TreeNode();
         temp.val = data;
         temp.left = temp.right = null;   //No child initially for the Binary tree
@@ -16,12 +14,24 @@ public class Opengenus_tree_random_node {
         return temp;
     }
 
-    static int getChild(TreeNode root)
-    {
+    static int getChild(TreeNode root) {
         if (root == null)
             return 0;
         return getChild(root.left) +
                 getChild(root.right) + 1;
     }
 
+    public static void main(String[] asdf) {
+        //Now let's create the binary Tree
+
+        TreeNode root = Node(1);
+        root.left = Node(2);
+        root.right = Node(3);
+        root.left.right = Node(12);
+        root.left.right = Node(89);
+        root.right.left = Node(68);
+        root.right.right = Node(99);
+        root.right.left = Node(68);
+
+    }
 }
