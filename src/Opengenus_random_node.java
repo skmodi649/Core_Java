eb4b5231a25b48aad61e0ca4ce96b4f87a55bfab
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-01-03 12:45:16 +0530

    Full explanation of the program commit

--- src/Opengenus_random_node.java
+++ src/Opengenus_random_node.java
@@ -1,3 +1,24 @@
+/** Author : Suraj Kumar
+ * Github : https://github.com/skmodi649
+ */
+
+/** PROBLEM DESCRIPTION :
+ * There is a Binary Search Tree given and we are supposed to find a random node in the given binary tree.
+ */
+
+/** ALGORITHM :
+ * Step 1: START
+ * Step 2: First create a binary tree using the steps mentioned in the first approach
+ * Step 3: Now use a method *inOrder()* that takes a node as input parameter to traverse through the
+ *         binary tree in inorder fashion as also store the values in a ArrayList simultaneously.
+ * Step 4: Now define a method *getrandom()* that takes a node as input parameter, in this first call
+ *         the inOrder() method to store the values in the arraylist, then find the size of the binary tree and now just generate a random number between 0 to n-1.
+ * Step 5: After generating the number display the value of the ArrayList at the generated index
+ * Step 6: STOP
+ */
+
+
+
 import java.util.*;
 // Let's define the class for the linked list
 
@@ -67,3 +88,21 @@ class Opengenus_random_node {
     }
 }
 
+/** Explanation of the Approach :
+ * (a) Form the required binary tree
+ * (b) Now use the *inOrder()* method to get the nodes in inOrder fashion and also store them in the given arraylist 'list'
+ * (c) Using the *getRandom()* method generate a random number between 0 to n-1, then get the value at the generated random number
+ *     from the arraylist using get() method and finally display the result.
+ */
+
+
+/** OUTPUT :
+ * First output :
+ * Random Node : 15
+ * Second output :
+ * Random Node : 99
+ */
+
+/** Time Complexity : O(n)
+ * Auxiliary Space Complexity : O(1)
+ */
