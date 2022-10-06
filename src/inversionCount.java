282f5a4114714304ce6776700438ac77a2cedb9e
Ashish <sahaashish807@gmail.com>
2022-10-06 09:09:59 +0530

    first commit slight change

--- src/inversionCount.java
+++ src/inversionCount.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class InversionCount {
     static int inversionCount(int arr[]){
-        int count=0;
+        int count_inversion=0;
         for(int i=0;i< arr.length-1;i++)
         {
             for(int j=i+1;j<arr.length;j++)
             {
                 if(arr[i]>arr[j])
-                    count++;
+                    count_inversion++;
             }
         }
-        return  count;
+        return count_inversion;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
